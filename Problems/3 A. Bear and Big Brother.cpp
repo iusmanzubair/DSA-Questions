85a07ed61844f1e31ad6789d9434d209ac8c766c
Usman <usmanzubair091@gmail.com>
2024-03-08 18:10:14 +0500

    Bear and Big Brother problem added

---
+++ Problems/3 A. Bear and Big Brother.cpp
@@ -0,0 +1,21 @@
+// Problem Link: https://codeforces.com/contest/791/problem/A
+
+// Solution:
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int limak_weight, bob_weight;
+    cin >> limak_weight >> bob_weight;
+
+    int count = 0;
+
+    while(limak_weight <= bob_weight) {
+        limak_weight *= 3;
+        bob_weight *= 2;
+
+        ++count;
+    }   
+    cout << count;
+}
