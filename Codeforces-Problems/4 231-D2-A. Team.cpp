bb7067e35446737f7e5ac4cab903d930dfd705f1
Usman <usmanzubair091@gmail.com>
2024-03-09 18:33:26 +0500

    Team Problem added

--- Codeforces-Problems/4 231-D2-A. Team.cpp
+++ Codeforces-Problems/4 231-D2-A. Team.cpp
@@ -4,7 +4,6 @@ using namespace std;
 int main() {
     int no_of_prob, ans = 0;
     cin >> no_of_prob;
-    
     for(int i=1; i<=no_of_prob; ++i) {
         int a, b, c;
         cin >> a >> b >> c;
