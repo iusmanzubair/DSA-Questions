c5e82051cfa0671f7051a808a3a914a71dd3dc48
Usman <usmanzubair091@gmail.com>
2024-03-15 14:45:40 +0500

    Petya and Strings problem added

--- Codeforces-Problems/5 405-D2-A. Gravity Flip.cpp
+++ Codeforces-Problems/5 405-D2-A. Gravity Flip.cpp
@@ -1,16 +1,18 @@
+// Problem Link: https://codeforces.com/contest/405/problem/A
+
+// Solution:
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    int n;
-    cin >> n;
+    int n; cin >> n;
     int arr[n];
     for (int i = 0; i < n; ++i)
         cin >> arr[i];
 
-    for (int i = 0; i < n - 1; ++i)
-    {
+    for (int i = 0; i < n - 1; ++i) {
         int minIndex = i;
         for (int j = i + 1; j < n; ++j)
         {
---
+++ Codeforces-Problems/6 112-D2-A. Petya and Strings.cpp
@@ -0,0 +1,33 @@
+// Problem Link: https://codeforces.com/contest/112/problem/A
+
+// Solution:
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int toLowerCase(int s) {
+    if(s >= 'A' && s <= 'Z')
+        s = s + 'a' - 'A';
+
+    return s;
+}
+
+int main() {
+  string s1, s2;
+    cin >> s1 >> s2;
+    int result;
+    for(int i=0; i<s1.length(); ++i) {
+        if(toLowerCase(s1[i]) > toLowerCase(s2[i])) {
+            result = 1;
+            break;
+        }
+        else if(toLowerCase(s1[i]) < toLowerCase(s2[i])) {
+            result = -1; 
+            break;
+        }
+        else
+            result = 0;
+    }
+
+    cout << result;
+}
