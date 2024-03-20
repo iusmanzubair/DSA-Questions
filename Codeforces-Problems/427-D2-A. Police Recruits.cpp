830e38e2258f7cba9c1ead9288d7414945a6b612
Usman <usmanzubair091@gmail.com>
2024-03-20 19:54:53 +0500

    Police Recruits problem added

---
+++ Codeforces-Problems/427-D2-A. Police Recruits.cpp
@@ -0,0 +1,24 @@
+// Problem Link: https://codeforces.com/contest/427/problem/A
+
+// Solution:
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    int x, recruits = 0, crime = 0;
+
+    for(int i=0; i<n; ++i) {
+        cin >> x;
+        if(x != -1)
+            recruits += x;
+        else if(recruits > 0)
+            recruits--;
+        else
+            crime++;
+    }
+
+    cout << crime;
+}
+
