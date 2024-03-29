be2091bcbf2a678ed6a6a6baafa9c4061784cc31
Usman <usmanzubair091@gmail.com>
2024-03-29 08:46:24 +0500

    Beautiful Matrix problem added

---
+++ Codeforces-Problems/14 263-D2-A Beautiful Matrix.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x, moves;
+    for(int i=1; i<=5; ++i) {
+        for(int j=1; j<=5; ++j) {
+            cin >> x;
+            if(x == 1) 
+                moves = abs(3-i) + abs(3-j);
+        }
+    }
+
+    cout << moves;
+}
