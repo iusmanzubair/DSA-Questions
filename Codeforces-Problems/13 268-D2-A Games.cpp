4d31336f6e9323b6b6eedf820ec791a2b0fd02fa
Usman <usmanzubair091@gmail.com>
2024-03-29 08:33:29 +0500

    Games problem added

---
+++ Codeforces-Problems/13 268-D2-A Games.cpp
@@ -0,0 +1,23 @@
+// Problem link: https://codeforces.com/contest/268/problem/A
+
+// Solution:
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    vector<int> h(n), a(n);
+    int number = 0;
+    for(int i=0; i<n; ++i) {
+        cin >> h[i] >> a[i];
+        for(int j=0; j<i; ++j) {
+            if(h[i] == a[j])
+                number++;
+            if(a[i] == h[j])
+                number++;
+        }
+    }
+
+    cout << number;
+}
