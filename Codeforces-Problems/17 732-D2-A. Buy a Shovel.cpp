4f3b9828cf23ed187ad8b347e5105b72f11c7835
Usman <usmanzubair091@gmail.com>
2024-04-02 12:12:05 +0500

    Buy a shovel problem added

---
+++ Codeforces-Problems/17 732-D2-A. Buy a Shovel.cpp
@@ -0,0 +1,17 @@
+// Problem Link: https://codeforces.com/contest/732/problem/A
+
+// Solution: 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int k, r; cin >> k >> r;
+    int i=1;
+    while(true) {
+        if((k*i)%10==0 || (k*i)%10==r) break;
+        i++;
+    }
+
+    cout << i;
+}
